d326c9341449fede35b46d55f11b89eea59b2193
umrswad <2250652265@qq.com>
2023-03-27 21:10:24 +0800

    cs

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <router-link to="/login">登录</router-link>
-        <router-link to="/main">主页</router-link>
+        <router-link to="/login">登录1</router-link>
+        <router-link to="/main">主页2</router-link>
         <router-view></router-view>
         <el-button type="success">Success</el-button>
     </div>
